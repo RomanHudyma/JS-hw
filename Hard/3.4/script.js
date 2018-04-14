a6abeebc7bf2ad6b8ce84e5fbc8bf46cd898df51
RomanHudyma <roman.hudymaa@gmail.com>
2018-04-14 22:38:40 +0300

    3.4 using better practices

--- Hard/3.4/script.js
+++ Hard/3.4/script.js
@@ -1,28 +1,18 @@
 var inputArr = [5, 10, 2, 3, 9, 47, 23, 4, 5, 5, 9, 7, 2, 9];
-var duplicateValues = [];
-function findDuplicateValues(){
+function findDuplicateValues(inputArr){
+	var duplicateValues = {};
+	var obj = [];
 	console.log(inputArr);
 	for (var i = 0; i < inputArr.length; i ++) {
 		for (var j = i + 1; j < inputArr.length; j++) {
-			if (inputArr[i] == inputArr[j] && !CheckIfPresent(inputArr[j])) {
-				duplicateValues.push(inputArr[j]);
+			if (inputArr[i] == inputArr[j] && !duplicateValues.hasOwnProperty(inputArr[i])) {
+				duplicateValues[inputArr[i]] = inputArr[i];
 				break;
 			}
 		}
 	}
-
-	console.log(duplicateValues);
-	return duplicateValues;
+	console.log(Object.keys(duplicateValues));
+	return Object.keys(duplicateValues);
 }
 
-function CheckIfPresent(number) {
-	for (var i = 0; i < duplicateValues.length; i++){
-		if (number == duplicateValues[i]) {
-			return true;
-		}
-	}
-
-	return false;
-}
-
-findDuplicateValues();
+findDuplicateValues(inputArr);
