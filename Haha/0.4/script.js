2430f02c9105af5791eeb50e336ce01264233a9f
RomanHudyma <roman.hudymaa@gmail.com>
2018-04-14 20:55:17 +0300

    0.4 Validation added

--- Haha/0.4/script.js
+++ Haha/0.4/script.js
@@ -1,5 +1,15 @@
-var date = prompt("Enter date:");
-var name = prompt("Enter name:");
-var city = prompt("Enter city:");
+while(true) {
 
-alert(name + " has a meeting " + date + " somewhere in " + city + ".");
+	var date = prompt("Enter date:");
+	var name = prompt("Enter name:");
+	var city = prompt("Enter city:");
+
+	var regExp_Name_City = /\D/;
+
+	if (!regExp_Name_City.test(name) || !regExp_Name_City.test(city)) {
+		alert("Your input is not valid! Try again)");
+	} else {
+		alert(name + " has a meeting " + date + " somewhere in " + city + ".");
+		break;
+	}
+}
