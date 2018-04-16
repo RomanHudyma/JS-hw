addb640f1ddde1be3927136da7e7464d36a3b532
Roman <roman.hudymaa@gmail.com>
2018-04-17 00:32:22 +0300

    Update script.js

--- Easy/1.2/script.js
+++ Easy/1.2/script.js
@@ -2,7 +2,7 @@ var inputArr = [66, 10, 15, 6, 25, 44, 3];
 function returnMinAndMax(inputArr) {
 	var max = inputArr[0];
 	var min = inputArr[0];
-	for (i=1; i < inputArr.length; i++) {
+	for (var i=1; i < inputArr.length; i++) {
 	  if (inputArr[i] > max) max = inputArr[i];
 	  if (inputArr[i] < min) min = inputArr[i];
 	}
